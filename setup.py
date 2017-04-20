07d2ca9dac0ddad32ae80905cb95e188d19f27c1
Claire Tang <clairetang6@gmail.com>
2017-04-20 15:37:23 -0700

    add setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name="intonatang",
+    description="Analysis code for speech intonation project",
+    version="2017.4.20",
+    author="Claire Tang",
+    author_email="clairetang6@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Visualization"
+    ]
+)
